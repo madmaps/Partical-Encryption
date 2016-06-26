503526daa2e9f8f66f4f6244bf6c2747f9b02c90
Matthew Applin <applin_matthew@yahoo.com>
2016-06-26 19:01:43 -0400

    almost

--- pencrypt.cpp
+++ pencrypt.cpp
@@ -1,17 +1,44 @@
 #include <iostream>
-#include "ParticalRandomGenerator.h"
+#include <string>
 #include <math.h>
+#include <fstream>
+#include "ParticalRandomGenerator.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-	ParticalRandomGenerator randGen("pasword","L4Jld2fis4IEls");
-	char temp ;
-	for(int i = 0; i<=10000;i++)
+	ifstream inFile(argv[1], ios::in|ios::binary|ios::ate);
+	ofstream outFile(argv[2], ios::out|ios::binary);
+	if(!inFile)
 	{
-		temp = randGen.getRandomChar();
-		cout << temp;
+		return 0;
+	}
+	streampos size = inFile.tellg();
+	inFile.seekg(0,ios::beg);
+	
+	
+	string password,password2;
+	cout << "Enter password: " << endl;
+	cin >> password;
+	cout << "Re-Enter password: " << endl;
+	cin >> password2;
+	if(password!=password2)
+	{
+		cout << "Passwords don't match" << endl;
+		return 0;
+	}
+	ParticalRandomGenerator randGen(password,"L4Jld2fis4IEls");
+	
+	
+	char temp[1] ;
+	char inFromFile[1];
+	for(int i = 0; i<size;i++)
+	{
+		inFile.read(inFromFile,1);
+		temp[0] = randGen.getRandomChar();
+		temp[0] = temp[0] ^ inFromFile[0];
+		outFile.write(temp,1);
 	}
 	return 0;
 }
